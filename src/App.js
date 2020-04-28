3130b0a2687cb3285c5fd0a1cb736231f238f1bb
Melvin Alfaro <a908rzz@mmm.com>
2020-04-28 09:35:57 -0600

    added new file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from 'react'; //en la sintaxis anterior, esto se hacía: import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component { // Component, que es una clase de React
+  render() { //como regla, un componente siempre tiene el rol de hace 'render' de al menos una cosa
+        return ( // es render, se hace por medio de un return, que retorna una pieza de HTML
+          <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            </header>
+          </div>)
+  }
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react'; //en la sintaxis anterior, esto se hacía: import React, { Component } from 'react';
+
+
+class App extends React.Component {
+	render() {
+		return <h1>Hello World</h1>
+	}
+}
+
+export default Hello; //se usa default, cuando solo se exporta una cosa.
